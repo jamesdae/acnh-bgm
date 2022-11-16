39a839b4d78d08cbaf5eccd5fe18daeaa1642132
James Yang <jamesdaeyang@gmail.com>
2022-11-16 01:18:41 +0000

    Added event handler for ended event to allow next audio element to play automatically. #1

--- js/main.js
+++ js/main.js
@@ -16,26 +16,33 @@ xhr.addEventListener('load', function () {
 
   for (var i = 0; i < arrayOfSongs.length; i++) {
     // bunch of switch case? for i when it gets to 18, 36, 24, or for if property at songs[i] has certain weather or number
-    if (arrayOfSongs[i].hour < 6) {
-      dawndusk.push(arrayOfSongs[i]);
-      var $listitem = document.createElement('li');
-      $listitem.setAttribute('class', 'row');
 
-      var $song = document.createElement('audio');
-      $song.setAttribute('controls', '');
-      $song.setAttribute('autoplay', '');
-      $song.setAttribute('loop', '');
-      $song.setAttribute('name', 'media');
+    dawndusk.push(arrayOfSongs[i]);
+    var $listitem = document.createElement('li');
+    $listitem.setAttribute('class', 'row list');
 
-      var $audio = document.createElement('source');
-      $audio.setAttribute('src', dawndusk[i].music_uri);
-      $audio.setAttribute('type', 'audio/mpeg');
+    var $songname = document.createElement('p');
+    var rawtitle = lastChars(8, arrayOfSongs[i]['file-name']);
+    var newTitle = lastChars(5, rawtitle) + ' ' + firstChars(2, rawtitle);
 
-      $main.appendChild($listitem);
-      $listitem.appendChild($song);
-      $song.appendChild($audio);
-      $song.addEventListener('play', pauseOthers);
-    }
+    $songname.textContent = newTitle;
+    $listitem.appendChild($songname);
+    var $song = document.createElement('audio');
+    $song.setAttribute('controls', '');
+    // $song.setAttribute('autoplay', '');
+    // $song.setAttribute('loop', '');
+    $song.setAttribute('name', 'media');
+
+    var $audio = document.createElement('source');
+    $audio.setAttribute('src', dawndusk[i].music_uri);
+    $audio.setAttribute('type', 'audio/mpeg');
+
+    $main.appendChild($listitem);
+    $listitem.appendChild($song);
+    $song.appendChild($audio);
+    $song.addEventListener('play', pauseOthers);
+    $song.addEventListener('ended', startNext);
+
   }
 });
 xhr.send();
@@ -47,4 +54,33 @@ function pauseOthers(event) {
       $songs[i].pause();
     }
   }
+}
+
+function startNext(event) {
+  var $songs = document.querySelectorAll('audio');
+  for (var i = 0; i < $songs.length; i++) {
+    if ($songs[i] === event.target) {
+      $songs[i + 1].play();
+    }
+  }
+}
+
+function firstChars(length, string) {
+  var newString = '';
+  if (length > string.length) {
+    return string;
+  }
+  for (var i = 0; i < string.length - (string.length - length); i++) {
+    newString += string[i];
+  } return newString;
+}
+
+function lastChars(length, string) {
+  var newString = '';
+  if (length > string.length) {
+    return string;
+  }
+  for (var i = string.length - length; i < string.length; i++) {
+    newString += string[i];
+  } return newString;
 }
