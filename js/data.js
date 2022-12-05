388da9c9f5a46cb8a4f3b3c3e583c0debefd7063
James Yang <jamesdaeyang@gmail.com>
2022-12-05 07:45:21 +0000

    Added ability to store changes to playlist in local storage, and general clean up.

--- js/data.js
+++ js/data.js
@@ -1,1 +1,17 @@
 /* exported data */
+
+var data = {
+  view: 'home',
+  entries: []
+};
+
+var previousdataJSON = localStorage.getItem('javascript-local-storage');
+
+if (previousdataJSON !== null) {
+  data = JSON.parse(previousdataJSON);
+}
+
+window.addEventListener('beforeunload', function (event) {
+  var dataJSON = JSON.stringify(data);
+  this.localStorage.setItem('javascript-local-storage', dataJSON);
+});
